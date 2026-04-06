18c469588e262738fb37dc6818e1dcdeca828d4b
Prabhat <prabhatprajapati988@gmail.com>
2026-04-06 19:10:08 +0530

    Updated API base URL using env

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,21 +1,10 @@
 import axios from "axios";
 
-const getBaseURL = () => {
-  const host = window.location.hostname;
-
-  // Phone / devtunnel / network access
-  if (host !== "localhost" && host !== "127.0.0.1") {
-    return "http://192.168.28.65:5000/api/v1";
-  }
-
-  // Laptop local
-  return "http://localhost:5000/api/v1";
-};
+const baseURL = `${import.meta.env.VITE_API_URL}/api/v1`;
 
 export const api = axios.create({
-  baseURL: getBaseURL(),
+  baseURL,
 });
-
 
 api.interceptors.request.use(
   (config) => {
