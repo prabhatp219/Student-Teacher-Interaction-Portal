10c7c2459bf8e562011402a7c4e1de4d8df06e34
Prabhat <prabhatprajapati988@gmail.com>
2026-04-06 23:39:54 +0530

    fix: routing for vercel

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -9,9 +9,14 @@ export const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-    if (token && !config.url.includes("/auth/login")) {
-      config.headers.Authorization = `Bearer ${token}`;
+
+    if (token) {
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${token}`,
+      };
     }
+
     return config;
   },
   (error) => Promise.reject(error)
